38a9b63934ba0e8954404b8e98cfe2978295afa6
liubingzhe-888 <liubingzhe666@126.com>
2021-04-29 14:44:41 +0800

    second

--- app/setting.py
+++ app/setting.py
@@ -1,14 +1,11 @@
 from app.web import web
 import os
-
 # 保存的文件格式
 SAVE_PATH = os.path.join(web.static_folder, "file")
-
 # 允许上传的文件格式
 # 可以在论文描述 作为论文内容
 # 比如：网盘提供了文件类型限制上传功能，旨在保证了非法文件的上传。
 ALLOWUPLOAD = ['.txt','.pdf','.doc','.docx','mp3','mp4','ppt','.zip','.rar','.jpg','.png','.jar']
-
 
 """
 设置网盘账号密码
